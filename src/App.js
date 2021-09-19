1388163cc478524ad85a07dd7a22425747ce9c90
Mohammad Abdur Rahman <rahmanriad.cse@gmail.com>
2021-09-19 12:57:22 +0600

    React Core Concepts Recap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyComponent></MyComponent>
+      <LoadUsers></LoadUsers>
+    </div>
+  );
+}
+
+function LoadUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUsers(data))
+  }, []);
+
+  return (
+    <div>
+      <h2>User Loaded: {users.length}</h2>
+      {
+        users.map(user => <User name={user.name} phone={user.phone} email={user.email} website={user.website}></User>)
+      }
+    </div>
+  );
+}
+
+function User(props) {
+  return (
+    <div className="user-design">
+      <h3>{props.name}</h3>
+      <p>Phone: {props.phone}</p>
+      <p>Email: {props.email}</p>
+      <p>Website: {props.website}</p>
+    </div>
+  );
+}
+
+function MyComponent() {
+  const [points, setPoints] = useState(1);
+
+  const handlePoints = () => {
+    const newPoints = points * 2;
+    setPoints(newPoints);
+  }
+  return (
+    <div>
+      <h2>Welcome to React World</h2>
+      <h4>I Have Point: {points}</h4>
+      <button onClick={handlePoints}>Add Points</button>
     </div>
   );
 }
